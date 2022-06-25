b04837428a385a467b0492e84a7b390d03a86730
Pavlo Kochuiev <pashadagilis@gmail.com>
2022-06-25 17:53:56 +0300

    Added Hold Shift to Check Multiple Checkboxes

--- 10 - Hold Shift to Check Multiple Checkboxes/script.js
+++ 10 - Hold Shift to Check Multiple Checkboxes/script.js
@@ -1,13 +1,18 @@
-"use strict";
+const checkboxes = document.querySelectorAll(".inbox");
 
-const checkboxes = document.querySelectorAll(`.inbox input[type="checkbox"]`);
+let lastChecked;
 
 function handleCheck(e) {
     let inBetween = false;
     if (e.shiftKey && this.checked) {
         checkboxes.forEach((checkbox) => {
-            if(checkbox === this || checkbox === lastChecked) {
+            console.log(checkbox);
+            if (checkbox === this || checkbox === lastChecked) {
                 inBetween = !inBetween;
+            }
+
+            if (inBetween) {
+                checkbox.checked = true;
             }
         });
     }
